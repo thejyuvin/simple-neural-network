edec4dea3a9be7d559586994f25d533fd340d55f
thej <@thej>
2017-07-02 17:31:34 -0500

    simple neural net starter

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple Neural Network
+A neural network written in Python, consisting of a single neuron that uses back propagation to learn.
+
+### Dependencies
+1. numpy
+
+### Tutorial:
+https://www.youtube.com/watch?v=p69khggr1Jo
+https://github.com/llSourcell/Make_a_neural_network
+
+### Challenge:
+Extend on the single layer feedforward neural network to create a 3 layer feedforward neural network using only numpy as dependency.
+
+### Goals:
+By doing this, you'll understand exactly how backpropagation works and develop an intuitive understanding of neural networks, which will be useful for more the more complex nets we build in the future. Backpropagation usually involves recursively taking derivatives, but in our 1 layer demo there was no recursion so was a trivial case of backpropagation. In this challenge, there will be. Use a small binary dataset, you can define one programmatically like in this example.
+
+#
---
+++ demo.py
@@ -0,0 +1,28 @@
+#pylint: skip-file
+from numpy import exp, array, random, dot
+from neural_network import NeuralNetwork
+
+
+if __name__ == "__main__":
+
+    #Intialise a single neuron neural network.
+    neural_network = NeuralNetwork()
+
+    print "Random starting synaptic weights: "
+    print neural_network.synaptic_weights
+
+    # The training set. We have 4 examples, each consisting of 3 input values
+    # and 1 output value.
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    # Train the neural network using a training set.
+    # Do it 10,000 times and make small adjustments each time.
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print "New synaptic weights after training: "
+    print neural_network.synaptic_weights
+
+    # Test the neural network with a new situation.
+    print "Considering new situation [1, 0, 0] -> ?: "
+    print neural_network.think(array([1, 0, 0]))
